b33119cb2c16293d85f0a46f2ad2b91a5097338b
Sally Yen <sally.yen@berkeley.edu>
2017-11-13 19:00:41 -0800

    created xcode proj

---
+++ whereToEat/whereToEat/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  whereToEat
+//
+//  Created by Sally Yen on 11/13/17.
+//  Copyright © 2017 Sally Yen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
